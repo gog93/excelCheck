b94d3e12574e6d9471d7a92ad08ac63e62e13a5f
gohar93 <zakaryangohar49@gmail.com>
2023-11-17 08:18:25 -0800

    LevenshteinDistance four commit

--- src/main/java/com/example/excelcheck/ExcelChecker.java
+++ src/main/java/com/example/excelcheck/ExcelChecker.java
@@ -1,18 +1,37 @@
 package com.example.excelcheck;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.text.similarity.LevenshteinDistance;
 
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
+@Controller
+@RequiredArgsConstructor
 public class ExcelChecker {
+    @Autowired
+    private AttachementUploadService attachmentService;
 
-    public static void main(String[] args) {
+    @GetMapping("/checkExcel")
+    public String firstStep(Model model) {
+        return "checkExcel";
+    }
+
+    @PostMapping("/fileUpload")
+
+    public String uploadAttachment(@RequestPart MultipartFile file, Model model) {
+        String examplFilePath = attachmentService.upload(file);
         try {
             FileInputStream excelFile1 = new FileInputStream("C:\\Users\\gohar\\IdeaProjects\\excelCheck\\example.xlsx");
             FileInputStream excelFile2 = new FileInputStream("C:\\Users\\gohar\\IdeaProjects\\excelCheck\\exampl.xlsx");
@@ -21,10 +40,14 @@ public class ExcelChecker {
             Map<String, Set<String>> stringListMap = symptomsAnalyse(excelFile1, columnNames);
             Map<String, Map<Integer, String>> stringMapMap = symptomsAnalyse(excelFile2, columnNames, stringListMap);
             Map<Integer, Map<String, String>> integerMapMap = levenshteinDistance(stringMapMap, stringListMap);
+        model.addAttribute("map",stringMapMap );
+        model.addAttribute("levenshteinDistance",integerMapMap );
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        return "checkExcel";
+
     }
 
     private static Map<String, Set<String>> symptomsAnalyse(FileInputStream excelFile, String[] columnNames) {
